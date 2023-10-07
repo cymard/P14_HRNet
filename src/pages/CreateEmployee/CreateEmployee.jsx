55c46416c6f7e63f90e42b3fced4a5dcf852a12b
cyrian simard <cyriansimard@gmail.com>
2023-10-07 18:54:50 +0200

    create modal + add modal style + use modal for a test

--- src/pages/CreateEmployee/CreateEmployee.jsx
+++ src/pages/CreateEmployee/CreateEmployee.jsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import Header from '../../components/Header/Header';
 import CreateEmployeeForm from '../../components/CreateEmployeeForm/CreateEmployeeForm.jsx';
 import { Link } from 'react-router-dom';
+import Modal from '../../components/Modal/Modal.jsx';
 
 const CreateEmployee = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    // TODO Enlever l'utilisation de modal ici
     return (
         <>
             <Header>HRnet</Header>
@@ -11,6 +15,14 @@ const CreateEmployee = () => {
             </Link>
             <div className="bg-light col-10 container px-4 py-3 rounded-4 my-4">
                 <h2>Create Employee</h2>
+                <Modal
+                    isOpen={isOpen}
+                    onClose={() => setIsOpen(false)}
+                    onExternalClick={() => setIsOpen(false)}
+                >
+                    Message de la modal
+                </Modal>
+                <button onClick={() => setIsOpen(true)}>Open modal</button>
                 <CreateEmployeeForm />
             </div>
             <div id="confirmation" className="modal">
